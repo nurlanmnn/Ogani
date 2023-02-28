972122d1b626e755a033db2d6e8ed85825fddd41
nurlanmnn <mmmdlinurlan40@gmail.com>
2023-02-28 19:12:26 +0400

    last

--- blog/translation.py
+++ blog/translation.py
@@ -1,13 +1,21 @@
 from modeltranslation.translator import translator, TranslationOptions
-
-from .models import Blog, News
+from .models import Blog, News, BlogCategory
 
 class BlogTranslationOptions(TranslationOptions):
     fields = ('title', 'description')
 
 
+class BlogCategoryTranslationOptions(TranslationOptions):
+    fileds = ('name')
+
+
 class NewsTranslationOptions(TranslationOptions):
     fields = ('title', 'description')
 
+
 translator.register(Blog, BlogTranslationOptions)
+translator.register(BlogCategory, BlogCategoryTranslationOptions)
 translator.register(News, NewsTranslationOptions)
+
+
+
--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 # from django.db.models import Q
 from .models import Product, Category
 from django.views.generic import ListView
+from decimal import Decimal
+
 
 # # Create your views here.
 
@@ -15,10 +17,16 @@ class ProductListView(ListView):
         queryset = super().get_queryset()
         product_name = self.request.GET.get('product_name')
         category_name = self.kwargs.get('category_name')
+        min_price = self.request.GET.get('min_price')
+        max_price = self.request.GET.get('max_price')
         if category_name:
             queryset = queryset.filter(category__name=category_name)
         if product_name:
             queryset = queryset.filter(title__icontains=product_name)
+        if min_price and max_price:
+            min_price_decimal = Decimal(min_price.replace('$', ''))
+            max_price_decimal = Decimal(max_price.replace('$', ''))
+            queryset = queryset.filter(price__gte=min_price_decimal, price__lte=max_price_decimal)
         return queryset
 
     def get_context_data(self, **kwargs):
@@ -26,6 +34,7 @@ class ProductListView(ListView):
         context['prod'] = Product.objects.all()
         context['categories'] = Category.objects.all()
         return context
+
 
 
 def sale(request):
@@ -47,5 +56,6 @@ def shoping_cart(request):
         'shoping_cart': shoping_cart
     }
     return render(request, 'shoping-cart.html', context)
+
 
 
